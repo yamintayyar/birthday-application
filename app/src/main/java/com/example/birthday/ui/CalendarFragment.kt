2a31999a78e389aaae56824e0600fe20f16649fa
yaminty <yamintyishere@gmail.com>
2022-04-18 18:00:07 -0400

    Sorted RecyclerView items
    Implemented sort into the RecyclerView item list by submitting the list of database items after they have been sorted chronologically and then in terms of if they have passed during the current year or not.
    
    For example, if the current date is March 20th, and two birthdays have been saved into the database, one on January 15th, and the other on March 25th, the RecyclerView will show the birthday on March 25th first, and then the birthday in January. This is in order to provide an better user experience.

--- app/src/main/java/com/example/birthday/ui/CalendarFragment.kt
+++ app/src/main/java/com/example/birthday/ui/CalendarFragment.kt
@@ -1,10 +1,14 @@
 package com.example.birthday.ui
 
+import android.icu.util.Calendar
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -12,6 +16,7 @@ import com.example.birthday.BirthdayApplication
 import com.example.birthday.BirthdayViewModel
 import com.example.birthday.BirthdayViewModelFactory
 import com.example.birthday.adapter.CalendarListAdapter
+import com.example.birthday.data.Birthday
 import com.example.birthday.databinding.FragmentCalendarBinding
 
 class CalendarFragment : Fragment() {
@@ -24,6 +29,11 @@ class CalendarFragment : Fragment() {
 
     private var _binding: FragmentCalendarBinding? = null
     private val binding get() = _binding!!
+
+    val dateComparator = Comparator { bday1: Birthday, bday2: Birthday ->
+        ((bday1.dateMonth * 30) + bday1.dateDay) - ((bday2.dateMonth * 30) + bday2.dateDay)
+        //assumes every month is 30 days long, and calculates which date comes before which
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,6 +48,7 @@ class CalendarFragment : Fragment() {
         return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -62,7 +73,32 @@ class CalendarFragment : Fragment() {
         recyclerView.adapter = adapter
 
         viewModel.allBirthdays.observe(viewLifecycleOwner) { birthdays -> //updates the recyclerview with any updates in the database
-            adapter.submitList(birthdays)
+
+            val sortedBirthdays = birthdays.sortedWith(dateComparator)
+
+            val calendar: Calendar =
+                Calendar.getInstance() //required to compare the current date against the now sorted list, to move them to the end
+            val currentDay = calendar.get(Calendar.DAY_OF_MONTH)
+            val currentMonth = calendar.get(Calendar.MONTH)
+            val comparableDate =
+                ((currentMonth + 1) * 30) + currentDay //has a date representing the current date that is comparable against the others
+
+            val displayList = mutableListOf<Birthday>()
+            val appendList = mutableListOf<Birthday>()
+
+            for (bday in sortedBirthdays) { //adds birthdays to the list to be displayed ONLY if they are still upcoming in the current year
+                if (((bday.dateMonth * 30) + bday.dateDay) < comparableDate) {
+                    appendList.add(bday)
+                } else {
+                    displayList.add(bday)
+                }
+            }
+
+            for (bday in appendList) { //adds the birthdays that have already passed in that current year
+                displayList.add(bday)
+            }
+
+            adapter.submitList(displayList)
         }
     }
 }
